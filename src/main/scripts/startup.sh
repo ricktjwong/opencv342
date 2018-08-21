f781b315e0d7545718622d3d516f0389af8a261a
Rick Wong <>
2018-08-21 12:14:29 +0800

    Revert "use new docker file, don't mount local file to container"
    This reverts commit dad01f65f26fd281f9eed37ce5d8d5f07aa5dc3e.

--- src/main/scripts/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Simple UNIX-ish script to start the job;
-# author : Rick Wong
-# date   : 20 Aug 2018
-#
-java -Djava.library.path=/opt/opencv_files -jar ../../../target/pHash-1.0.0.jar
